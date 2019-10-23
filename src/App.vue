d6716074a2e5d6f90d736ff3efb47cd9cdc7c8b0
Steven Childrey <steven.childrey36@yahoo.com>
2019-10-23 17:47:55 -0400

    Set up simple structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hamburger Customizer</h1>
+    <Hamburger />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hamburger from './components/Hamburger.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Hamburger
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #120029;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Hamburger.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hamburger">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    lineHeight: Number,
+    lineWidth: Number,
+    lineSpacing: Number,
+    lineRadius: Number,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

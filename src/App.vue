4d9973d0d2055f6c0c989781811277955474fc6f
Steven Childrey <steven.childrey36@yahoo.com>
2019-10-23 20:08:04 -0400

    Create style copy area

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <h1 class="title">Hamburger Customizer</h1>
     <Hamburger :lineHeight=5 :lineWidth=35 :lineSpacing=7 :lineRadius=5 :lineColor="c0ff33" />
+    <Style :lineHeight=5 :lineWidth=35 :lineSpacing=7 :lineRadius=5 :lineColor="c0ff33" />
   </div>
 </template>
 
 <script>
 import Hamburger from './components/Hamburger.vue'
+import Style from './components/Style.vue'
 
 export default {
   name: 'app',
   components: {
-    Hamburger
+    Hamburger,
+    Style,
   }
 }
 </script>
---
+++ src/components/Style.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="style">
+<h3>CSS</h3>
+<pre>
+<code>.hamburger .span {
+  height: {{ lineHeight }}px;
+  width: {{ lineWidth }}px;
+  margin-bottom: {{ lineSpacing }}px;
+  border-radius: {{ lineRadius }}px;
+}
+
+.hamburger .span:last-child {
+  margin-bottom: 0;
+}</code>
+</pre>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    lineHeight: Number,
+    lineWidth: Number,
+    lineSpacing: Number,
+    lineRadius: Number,
+    lineColor: String,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>

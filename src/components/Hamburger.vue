2df6afc8a36062cc6d394897ee207289e2a1b619
Steven Childrey <steven.childrey36@yahoo.com>
2019-10-23 20:07:50 -0400

    Add click listener to toggle class

--- src/components/Hamburger.vue
+++ src/components/Hamburger.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="hamburger">
-    <button>
+  <div class="container">
+    <button @click="isOpen = !isOpen" :class="{open: isOpen}">
       <span :style="{ backgroundColor: '#' + lineColor, height: lineHeight + 'px', width: lineWidth + 'px', borderRadius: lineRadius + 'px', marginBottom: lineSpacing + 'px' }"></span>
       <span :style="{ backgroundColor: '#' + lineColor, height: lineHeight + 'px', width: lineWidth + 'px', borderRadius: lineRadius + 'px', marginBottom: lineSpacing + 'px' }"></span>
       <span :style="{ backgroundColor: '#' + lineColor, height: lineHeight + 'px', width: lineWidth + 'px', borderRadius: lineRadius + 'px',  }"></span>
     </button>
   </div>
-
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      isOpen: false,
+    }
+  },
   props: {
     lineHeight: Number,
     lineWidth: Number,
@@ -18,14 +22,20 @@ export default {
     lineRadius: Number,
     lineColor: String,
   },
-  computed: {
-
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    padding: 2rem 0;
+    border: 1px solid hsl(0, 0%, 83%);
+    min-height: 200px;
+    max-height: 400px;
+  }
+
   button {
     background-color: transparent;
     padding: 0;
@@ -39,5 +49,9 @@ export default {
   span {
     display: block;
     background-color: red;
+  }
+
+  .open span {
+    background-color: purple;
   }
 </style>

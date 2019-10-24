3f7e1a02bdf486f9b53c1c1539fac303f905f046
Steven Childrey <steven.childrey36@yahoo.com>
2019-10-23 22:35:36 -0400

    Set up controls

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
   <div id="app">
     <h1 class="title">Hamburger Customizer</h1>
-    <Hamburger :lineHeight=5 :lineWidth=35 :lineSpacing=7 :lineRadius=5 :lineColor="'c0ff33'" />
-    <Style :lineHeight=5 :lineWidth=35 :lineSpacing=7 :lineColor="'c0ff33'" :lineRadius=5 />
+    <Hamburger :lineHeight="height" :lineWidth="width" :lineSpacing="spacing" :lineRadius="radius" :lineColor="color" />
+    <Controls class="style" v-on:updateHeight="heightUpdate" v-on:updateWidth="widthUpdate" v-on:updateRadius="radiusUpdate" v-on:updateSpacing="spacingUpdate" />
+    <Style :lineHeight="height" :lineWidth="width" :lineSpacing="spacing" :lineColor="color" :lineRadius="radius" />
   </div>
 </template>
 
 <script>
 import Hamburger from './components/Hamburger.vue'
+import Controls from './components/Controls.vue'
 import Style from './components/Style.vue'
 
 export default {
   name: 'app',
   components: {
     Hamburger,
+    Controls,
     Style,
+  },
+  data () {
+    return {
+      height: 5,
+      width: 35,
+      spacing: 7,
+      radius: 5,
+      color: 'c0ff33',
+    }
+  },
+  methods: {
+    heightUpdate: function(value) {
+      this.height = value;
+    },
+    widthUpdate: function(value) {
+      this.width = value;
+    },
+    radiusUpdate: function(value) {
+      this.radius = value;
+    },
+    spacingUpdate: function(value) {
+      this.spacing = value;
+    },
   }
 }
 </script>
@@ -29,5 +55,8 @@ export default {
 }
 .title {
   text-align: center;
+}
+.style {
+  margin-top: 20px;
 }
 </style>
---
+++ src/components/Controls.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <label for="lineHeight">Line Height</label>
+    <input @input="log('updateHeight')" v-model="lineHeight" type="range" name="lineHeight" min="0" max="50">
+    <label for="lineWidth">Line Width</label>
+    <input @input="log('updateWidth')" v-model="lineWidth" type="range" name="lineWidth" id="" min="0" max="400">
+    <label for="lineRadius">Line Border Radius</label>
+    <input @input="log('updateRadius')" v-model="lineRadius" type="range" name="lineRadius" id="" min="0" max="50">
+    <label for="lineSpacing">Line Spacing</label>
+    <input @input="log('updateSpacing')" v-model="lineSpacing" type="range" name="lineSpacing" id="" min="0" max="50">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      lineHeight: 5,
+      lineWidth: 35,
+      lineSpacing: 7,
+      lineRadius: 5,
+      lineColor: 'c0ff33',
+    }
+  },
+  methods: {
+    log: function(valueToChange) {
+      if (valueToChange == 'updateHeight') {
+        this.$emit(valueToChange, this.lineHeight);
+      }
+      else if (valueToChange == 'updateWidth') {
+        this.$emit(valueToChange, this.lineWidth);
+      }
+      else if (valueToChange == 'updateRadius') {
+        this.$emit(valueToChange, this.lineRadius);
+      }
+      else {
+        this.$emit(valueToChange, this.lineSpacing);
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+</style>

bdd1b1657c8eb47f8c3d119232b13ec546d366ee
Steven Childrey <steven.childrey36@yahoo.com>
2019-10-28 15:04:08 -0400

    Set up styles copy area

--- src/components/Style.vue
+++ src/components/Style.vue
@@ -2,16 +2,67 @@
 <div class="style">
 <h3>CSS</h3>
 <pre>
-<code>.hamburger .span {
+<code>.menu-toggle {
+  padding: 0.3rem;
+  background-color: transparent;
+  border: none;
+  z-index: 999;
+}
+
+.menu-toggle:hover {
+  cursor: pointer;
+}
+
+.menu-toggle span {
+  display: block;
   background-color: {{ lineColor }};
   height: {{ lineHeight }}px;
   width: {{ lineWidth }}px;
   margin-bottom: {{ lineSpacing }}px;
   border-radius: {{ lineRadius }}px;
+  position: relative;
+  transition:
+    top 0.3s 0.3s ease-in-out,
+    bottom 0.3s 0.3s ease-in-out,
+    transform 0.3s,
+    opacity 0s 0.3s ease-in-out;
 }
 
-.hamburger .span:last-child {
+.menu-toggle.open span {
+  transition:
+    top 0.3s ease-in-out,
+    bottom 0.3s ease-in-out,
+    transform 0.3s 0.3s,
+    opacity 0s 0.3s ease-in-out;
+}
+
+.menu-toggle span:first-child {
+  top: 0;
+}
+
+.menu-toggle span:nth-child(2) {
+  opacity: 1;
+}
+
+.menu-toggle span:last-child {
+  bottom: 0;
   margin-bottom: 0;
+}
+
+.menu-toggle.open span:first-child {
+  transform: rotate(45deg);
+  transform-origin: center;
+  top: {{ Number(lineHeight) + Number(lineSpacing) }}px;
+}
+
+.menu-toggle.open span:nth-child(2) {
+  opacity: 0;
+}
+
+.menu-toggle.open span:last-child {
+  transform: rotate(-45deg);
+  transform-origin: center;
+  bottom: {{ Number(lineHeight) + Number(lineSpacing) }}px;
 }</code>
 </pre>
 </div>

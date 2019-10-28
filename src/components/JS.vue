eee15c4ac5b18d974693037df9a3dec002089c42
Steven Childrey <steven.childrey36@yahoo.com>
2019-10-28 15:59:50 -0400

    Add JS component

---
+++ src/components/JS.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="style">
+<h3>JavaScript</h3>
+<pre>
+<code>const menuToggle = document.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', function() {
+  menuToggle.classList.toggle('open');
+  // Add things here to toggle your actual menu
+});</code>
+</pre>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  code {
+    display: block;
+    max-width: 100%;
+    max-height: 400px;
+    height: 400px;
+    overflow: auto;
+    padding: 0.2rem;
+    outline: 1px solid hsla(0, 0%, 83%, 25%);
+    background-color: rgb(243, 243, 243);
+  }
+
+  @media screen and (max-width: 767px) {
+    code {
+      height: auto;
+    }
+  }
+</style>

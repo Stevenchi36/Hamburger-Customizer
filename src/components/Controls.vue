9117bbb15c3491b37211c2a7c4fdddf336e3b084
Steven Childrey <steven.childrey36@yahoo.com>
2019-10-28 15:04:27 -0400

    Style controls and add ids

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container">
+    <label for="lineColor">Color</label>
+    <input @input="log('updateColor')" type="color" v-model="lineColor" name="lineColor" id="lineColor">
     <label for="lineHeight">Line Height</label>
-    <input @input="log('updateHeight')" v-model="lineHeight" type="range" name="lineHeight" min="0" max="50">
+    <input @input="log('updateHeight')" v-model="lineHeight" type="range" name="lineHeight" id="lineHeight" min="0" max="50">
     <label for="lineWidth">Line Width</label>
-    <input @input="log('updateWidth')" v-model="lineWidth" type="range" name="lineWidth" id="" min="0" max="400">
+    <input @input="log('updateWidth')" v-model="lineWidth" type="range" name="lineWidth" id="lineWidth" min="0" max="400">
     <label for="lineRadius">Line Border Radius</label>
-    <input @input="log('updateRadius')" v-model="lineRadius" type="range" name="lineRadius" id="" min="0" max="50">
+    <input @input="log('updateRadius')" v-model="lineRadius" type="range" name="lineRadius" id="lineRadius" min="0" max="50">
     <label for="lineSpacing">Line Spacing</label>
-    <input @input="log('updateSpacing')" v-model="lineSpacing" type="range" name="lineSpacing" id="" min="0" max="50">
-    <label for="lineSpacing">Color</label>
-    <input @input="log('updateColor')" type="color" v-model="lineColor" name="lineColor" id="">
+    <input @input="log('updateSpacing')" v-model="lineSpacing" type="range" name="lineSpacing" id="lineSpacing" min="0" max="50">
   </div>
 </template>
 
@@ -52,5 +52,91 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
+  }
+
+  input[type=color] {
+    margin: 0.4rem 0 0.8rem;
+  }
+  input[type=range] {
+    -webkit-appearance: none;
+    margin: 0.8rem 0;
+    width: 100%;
+    background-color: transparent;
+  }
+  input[type=range]::-moz-range-track {
+    width: 100%;
+    height: 6px;
+    cursor: pointer;
+    animate: 0.2s;
+    background: #ffffff;
+    border-radius: 1.3px;
+    border: 0.2px solid #010101;
+  }
+  input[type=range]::-moz-range-thumb {
+    border: 1px solid #000000;
+    height: 15px;
+    width: 10px;
+    border-radius: 3px;
+    background: #120029;
+    cursor: pointer;
+  }
+  input[type=range]:focus {
+    outline: none;
+  }
+  input[type=range]::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    animate: 0.2s;
+    background: #3071a9;
+    border-radius: 1.3px;
+    border: 0.2px solid #010101;
+  }
+  input[type=range]::-webkit-slider-thumb {
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ffffff;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -14px;
+  }
+  input[type=range]:focus::-webkit-slider-runnable-track {
+    background: #367ebd;
+  }
+  input[type=range]::-ms-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    animate: 0.2s;
+    background: transparent;
+    border-color: transparent;
+    border-width: 16px 0;
+    color: transparent;
+  }
+  input[type=range]::-ms-fill-lower {
+    background: #2a6495;
+    border: 0.2px solid #010101;
+    border-radius: 2.6px;
+  }
+  input[type=range]::-ms-fill-upper {
+    background: #3071a9;
+    border: 0.2px solid #010101;
+    border-radius: 2.6px;
+  }
+  input[type=range]::-ms-thumb {
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ffffff;
+    cursor: pointer;
+  }
+  input[type=range]:focus::-ms-fill-lower {
+    background: #3071a9;
+  }
+  input[type=range]:focus::-ms-fill-upper {
+    background: #367ebd;
   }
 </style>
